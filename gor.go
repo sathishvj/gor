3fb7f3024c94766a0bbc9817047eff018365259d
sathishvj <sathishvj@yahoo.com>
2013-09-29 19:08:47 +0530

    Made it work for channels. Also using channels for stdout and stderr.

--- gor.go
+++ gor.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"bytes"
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
-	"log"
 	"os"
 	"os/exec"
 	"os/user"
@@ -21,7 +21,7 @@ func main() {
 		fmt.Println("Error getting current user.", err.Error())
 		return
 	}
-	log.Println("HomeDir is:", u.HomeDir)
+	//fmt.Println("HomeDir is:", u.HomeDir)
 
 	gorFile := u.HomeDir + string(os.PathSeparator) + ".gorfile"
 	_, err = os.Stat(gorFile)
@@ -36,7 +36,7 @@ func main() {
 			panic(err)
 		}
 		gorList = string(b)
-		fmt.Println("gorfile contains::", gorList)
+		//fmt.Println("gorfile contains::", gorList)
 	}
 
 	list := strings.Split(gorList, ":")
@@ -49,7 +49,7 @@ func main() {
 	}
 
 	files := flag.Args()
-	fmt.Println("Command line files are:", files)
+	//fmt.Println("Command line files are:", files)
 	if len(files) == 0 && len(gorList) == 0 {
 		fmt.Println("Err! One go file needs to be given as there was nothing in the previous list either.")
 		return
@@ -70,18 +70,76 @@ func main() {
 	} else {
 		runFile = list[len(list)-1]
 	}
-	fmt.Println("Runfile is:", runFile)
+	//fmt.Println("Runfile is:", runFile)
 
-	err = ioutil.WriteFile(gorFile, []byte(gorList), 0644)
+	// for now, just write the last one.
+	//err = ioutil.WriteFile(gorFile, []byte(gorList), 0644)
+	err = ioutil.WriteFile(gorFile, []byte(runFile), 0644)
 
-	fmt.Println("Going to run:", runFile)
+	fmt.Println("Going to run:", runFile, "...")
 
 	cmd := exec.Command("go", "run", runFile)
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	err = cmd.Run()
+	stdout, err := cmd.StdoutPipe()
 	if err != nil {
+		fmt.Println("Error getting StdoutPipe")
 		panic(err)
 	}
-	fmt.Println(out.String())
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		fmt.Println("Error getting StdErrPipe")
+		panic(err)
+	}
+
+	outPipe := bufio.NewReader(stdout)
+	errPipe := bufio.NewReader(stderr)
+	outCh := make(chan string)
+	errCh := make(chan string)
+
+	err = cmd.Start()
+	if err != nil {
+		fmt.Println("Error starting cmd")
+		panic(err)
+	}
+	go getStdOutput(outCh, outPipe)
+	go getStdOutput(errCh, errPipe)
+outside:
+	for {
+		select {
+		case s, ok := <-outCh:
+			if !ok {
+				break outside
+			}
+			//fmt.Println("From Out:", s)
+			fmt.Println(s)
+		case s, ok := <-errCh:
+			if !ok {
+				break outside
+			}
+			fmt.Println("Err!", s)
+		}
+	}
+	//fmt.Println("Finished")
+
+	err = cmd.Wait()
+	if err != nil {
+		fmt.Println("Waiting error", err)
+		return
+	}
+}
+
+func getStdOutput(c chan string, p *bufio.Reader) {
+	for {
+		line, _, err := p.ReadLine()
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println("ReadLine error:", err)
+				//return
+			}
+			break
+		}
+		//fmt.Println("Got line:", string(line))
+		c <- string(line)
+	}
+	//fmt.Println("Closing c")
+	close(c)
 }
