d087d50e0fad6feb5151dbd62f8fb01201a80865
sathishvj <sathishvj@yahoo.com>
2013-09-30 01:38:28 +0530

    Per directory + per tool values.

--- README.md
+++ README.md
@@ -2,8 +2,9 @@ gor
 ===
 
 ## Execute previous go run command
-To gain a few seconds from repeatedly retyping "go run filename.go".
-This creates a .gorfile in your user directory that contains the last run file.  Thereafter, just run gor.
+When learning go or running trials with small programs, one usually has a few different .go files in the same directory.   
+
+This program creates a .gorrc in your user directory that contains the last run/build/test file for your present working directory.  Thereafter, just run gor (or with -b/-t option for build and test respectively).  An entry exists per directory and per tool (run/build/test/doc) that you use.
 
 ## Installation
 ```
@@ -12,16 +13,34 @@ go get github.com/sathishvj/gor
 
 > This will pull down the source and install the gor command in $GOROOT/bin
 
-## Running it
+## Running gor
 Make sure that your PATH contains $GOPATH/bin.  Then use gor from any directory.
 
 ```
 gor hello.go 
 ```
-> this will run hello.go for the first time and add it to .gorfile in your user folder.
+> this will run hello.go for the first time and add it to .gorrc in your user folder.
 
 ```
 gor
 ```
 
 > this will re-run hello.go
+
+```
+gor -b hello.go
+```
+
+> this will build hello.go for the first time.
+
+```
+gor -b 
+```
+
+> this will re-build hello.go
+
+Tip: After using "gor -b" for the first time, you might want to alias gob so that you can use just gob each time.  Since alias cannot accept parameters though, for the first time you build a file, you will have to use "gor -b"
+```
+alias gob="gor -b"
+```
+
--- gor.go
+++ gor.go
@@ -13,8 +13,37 @@ import (
 )
 
 func main() {
-	listAll := flag.Bool("l", false, "-l")
+	listAll := flag.Bool("l", false, "to list all entries")
+	help := flag.Bool("h", false, "to display this usage listing")
+	curr := flag.Bool("c", false, "to show go file for current directory")
+	rem := flag.Bool("r", false, "to remove the entry corresponding to current directory")
+	del := flag.Bool("x", false, "to delete the current .gorrc")
+	build := flag.Bool("b", false, "to use build tool (default is run)")
+	test := flag.Bool("t", false, "to use test tool (default is run)")
+	doc := flag.Bool("d", false, "to use test tool (default is run)")
 	flag.Parse()
+	anyFlags := *listAll || *help || *curr || *rem || *del
+
+	if (*build && *test) || (*test && *doc) || (*build && *doc) {
+		fmt.Println("Error: You can have only one of build/test/doc.\n")
+		usage()
+		return
+	}
+	tool := "run"
+	if *build {
+		tool = "build"
+	}
+	if *test {
+		tool = "test"
+	}
+	if *doc {
+		tool = "doc"
+	}
+
+	if *help {
+		usage()
+		return
+	}
 
 	u, err := user.Current()
 	if err != nil {
@@ -23,62 +52,129 @@ func main() {
 	}
 	//fmt.Println("HomeDir is:", u.HomeDir)
 
-	gorFile := u.HomeDir + string(os.PathSeparator) + ".gorfile"
-	_, err = os.Stat(gorFile)
-	var gorList string
+	gorrc := u.HomeDir + string(os.PathSeparator) + ".gorrc"
+	gorExists := true
+	_, err = os.Stat(gorrc)
 	if err != nil {
-		if !os.IsNotExist(err) {
-			panic(err)
+		if os.IsNotExist(err) {
+			gorExists = false
+		} else {
+			fmt.Println("Error reading file", gorrc, ":", err)
+			return
 		}
-	} else {
-		b, err := ioutil.ReadFile(gorFile)
+	}
+
+	if *del {
+		if !gorExists {
+			return
+		}
+		err = os.Remove(gorrc)
 		if err != nil {
-			panic(err)
+			fmt.Println("Error removing file", gorrc, ":", err)
+		} else {
+			fmt.Println("Removed", gorrc)
+		}
+		return
+	}
+
+	var gorList string
+	if gorExists {
+		b, err := ioutil.ReadFile(gorrc)
+		if err != nil {
+			fmt.Println("Error reading file", gorrc, ":", err)
 		}
 		gorList = string(b)
 		//fmt.Println("gorfile contains::", gorList)
 	}
 
-	list := strings.Split(gorList, ":")
+	m := make(map[string]map[string]string)
+	lines := strings.Split(gorList, "\n")
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		if len(line) == 0 {
+			continue
+		}
+		parts := strings.Split(line, "=")
+		if _, exists := m[parts[0]]; exists {
+			m[parts[0]][parts[1]] = parts[2]
+		} else {
+			tmpM := make(map[string]string)
+			tmpM[parts[1]] = parts[2] //run = file
+			m[parts[0]] = tmpM        //dir=run=file
+		}
+	}
 
 	if *listAll {
-		for i := 0; i < len(list) && len(list) > 0; i++ {
-			fmt.Println(i+1, ":", list[i])
+		if !gorExists {
+			return
+		}
+		for k, v := range m {
+			for k1, v1 := range v {
+				fmt.Println(k, "=", k1, "=", v1)
+			}
 		}
 		return
 	}
 
-	files := flag.Args()
-	//fmt.Println("Command line files are:", files)
-	if len(files) == 0 && len(gorList) == 0 {
-		fmt.Println("Err! One go file needs to be given as there was nothing in the previous list either.")
+	pwd, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Error getting working directory.", err.Error())
 		return
 	}
-	if len(files) > 1 {
-		fmt.Println("Err! Specify only one file please.")
+	//fmt.Println("pwd is:", pwd)
+
+	if *rem {
+		delete(m[pwd], tool)
+		write(m, gorrc)
 		return
 	}
 
-	var runFile string
-
-	if len(files) != 0 {
-		if len(gorList) > 0 {
-			gorList += ":"
+	if *curr {
+		tmpM, exists := m[pwd]
+		if !exists {
+			return
 		}
-		gorList += files[0]
-		runFile = files[0]
-	} else {
-		runFile = list[len(list)-1]
+		for k, v := range tmpM {
+			fmt.Println(pwd, "=", k, "=", v)
+		}
+		return
 	}
-	//fmt.Println("Runfile is:", runFile)
 
-	// for now, just write the last one.
-	//err = ioutil.WriteFile(gorFile, []byte(gorList), 0644)
-	err = ioutil.WriteFile(gorFile, []byte(runFile), 0644)
+	// if any flags are on, then it is just a options setting request
+	if anyFlags {
+		return
+	}
 
-	fmt.Println("Going to run:", runFile, "...")
+	var runArgs string
+	var ok bool
+	runArgs, ok = m[pwd][tool]
+	if len(flag.Args()) == 0 && !ok {
+		fmt.Println("Error! No arguments given and no previous entry in gorfile.\n")
+		usage()
+		return
+	} else if len(flag.Args()) > 0 {
+		runArgs = strings.Join(flag.Args(), " ")
+	}
 
-	cmd := exec.Command("go", "run", runFile)
+	//fmt.Println("Runargs is:", runArgs)
+
+	if !ok || len(flag.Args()) > 0 { //this entry didn't exist before
+		if _, exists := m[pwd]; exists {
+			m[pwd][tool] = runArgs
+		} else {
+			tmpM := make(map[string]string)
+			tmpM[tool] = runArgs //run = file
+			m[pwd] = tmpM        //dir=run=file
+		}
+		err = write(m, gorrc)
+		if err != nil {
+			fmt.Println("Err! Unable to write to ", gorrc, ":", err)
+			return
+		}
+	}
+
+	fmt.Println("go", tool, runArgs)
+	cmd := exec.Command("go", tool, runArgs)
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
 		fmt.Println("Error getting StdoutPipe")
@@ -123,7 +219,8 @@ outside:
 
 	err = cmd.Wait()
 	if err != nil {
-		fmt.Println("Waiting error", err)
+		// Typically a compile error. Don't print any message.
+		//fmt.Println("Waiting error", err)
 		return
 	}
 }
@@ -143,4 +240,26 @@ func getStdOutput(c chan string, p *bufio.Reader) {
 	}
 	//fmt.Println("Closing c")
 	close(c)
+}
+
+func usage() {
+	pgm := os.Args[0]
+	fmt.Printf("%s re-runs or re-builds  previously used go file in the current directory.  Usage:\n", pgm)
+	flag.PrintDefaults()
+	fmt.Printf("\n%s hello.go\n%s\n", pgm, pgm)
+	return
+}
+
+func write(m map[string]map[string]string, f string) error {
+	var s string
+	for k, v := range m {
+		for k1, v1 := range v {
+			s = s + k + "=" + k1 + "=" + v1 + "\n"
+		}
+	}
+	err := ioutil.WriteFile(f, []byte(s), 0644)
+	if err != nil {
+		return err
+	}
+	return nil
 }
